2f1ec04229a37294a19f5663fc9c1daa22bd3bf7
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 22:33:37 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,4 +18,10 @@ class Waiter
     Meal.new(self, customer, total, tip)
   end
 
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+    end
+  end
+
 end
